a9bfb74db26035cd9dba20e221906c54cc0b3f87
Ong Zi Yang <celeskyanpu@gmail.com>
2024-09-25 08:18:52 +0800

    reviews csv added but bugged
    ratings are throwing invalid args

--- arrayA2celes.cpp
+++ arrayA2celes.cpp
@@ -1,11 +1,13 @@
 #include "arrays.hpp"
 #include <iostream>
 #include <fstream>
+#include <sstream>
 
 FileReader::FileReader()
 {
     negativeRead = 0;
     positiveRead = 0;
+    reviewsRead = 0;
 }
 
 void FileReader::readNegative(const std::string &filename)
@@ -50,12 +52,51 @@ void FileReader::readPositive(const std::string &filename)
     }
 };
 
+void FileReader::readReviews(const std::string &filename)
+{
+    std::ifstream file("C:/Github/dstr-assignment/required/tripadvisor_hotel_reviews.csv");
+    if (file.is_open())
+    {
+        std::string line, review;
+        int rating;
+        std::getline(file, line);
+
+        while (std::getline(file, line) && reviewsRead < csv_array_size)
+        {
+            std::stringstream ss(line);
+            std::getline(ss, review, ',');
+
+            std::string rating_str;
+            std::getline(ss, rating_str);
+
+            try
+            {
+                rating = std::stoi(rating_str);
+            }
+            catch (const std::invalid_argument &)
+            {
+                rating = 0;
+            }
+
+            reviews[reviewsRead] = review;
+            ratings[reviewsRead] = rating;
+            reviewsRead++;
+        }
+        file.close();
+    }
+    else
+    {
+        std::cerr << "Error, file cannot be opened." << std::endl;
+    }
+}
+
 int main()
 {
     FileReader reader;
 
     reader.readNegative("C:/Github/dstr-assignment/required/negative-words.txt");
     reader.readPositive("C:/Github/dstr-assignment/required/positive-words.txt");
+    reader.readReviews("C:/Github/dstr-assignment/required/tripadvisor_hotel_reviews.csv");
 
     std::cout << "Negative Words:" << std::endl;
     for (int i = 0; i < reader.negativeRead; i++)
@@ -67,6 +108,12 @@ int main()
     for (int i = 0; i < reader.positiveRead; i++)
     {
         std::cout << reader.positiveWords[i] << std::endl;
+    }
+
+    std::cout << "\nReviews:" << std::endl;
+    for (int i = 0; i < reader.reviewsRead; i++)
+    {
+        std::cout << "Reviews: " << reader.reviews[i] << ", Rating: " << reader.ratings[i] << std::endl;
     }
 
     return 0;
--- arrays.hpp
+++ arrays.hpp
@@ -7,15 +7,21 @@ class FileReader
 {
 public:
     static const int array_size = 5000;
+    static const int csv_array_size = 21000;
     std::string negativeWords[array_size];
     std::string positiveWords[array_size];
+    std::string reviews[csv_array_size];
+    int ratings[csv_array_size];
+
     int negativeRead = 0;
     int positiveRead = 0;
+    int reviewsRead = 0;
 
     FileReader();
 
     void readNegative(const std::string &filename);
     void readPositive(const std::string &filename);
+    void readReviews(const std::string &filename);
 };
 
 #endif
