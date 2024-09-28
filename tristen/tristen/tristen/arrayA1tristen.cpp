af0db12c0a14afc13909dba7e39590e408f53373
Tristen Chris Melvin Mojinun <tristenchris@gmail.com>
2024-09-28 13:32:22 +0800

    adding cpp & hpp file for array

---
+++ tristen/tristen/tristen/arrayA1tristen.cpp
@@ -0,0 +1,6 @@
+#include "arrays.hpp"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <cctype>
---
+++ tristen/tristen/tristen/arrays.hpp
@@ -0,0 +1,28 @@
+#ifndef READFILE_HPP
+#define READFILE_HPP
+using namespace std;
+#include <string>
+
+class FileReader
+{
+public:
+    static const int array_size = 5000;
+    static const int csv_array_size = 21000;
+    std::string negativeWords[array_size];
+    std::string positiveWords[array_size];
+    std::string reviews[csv_array_size];
+    int ratings[csv_array_size];
+
+    int negativeRead = 0;
+    int positiveRead = 0;
+    int reviewsRead = 0;
+
+    FileReader();
+
+    void readNegative(const std::string& filename);
+    void readPositive(const std::string& filename);
+
+    std::string** readCSV(const std::string& filename, int& numRows, int& numCols);
+};
+
+#endif
